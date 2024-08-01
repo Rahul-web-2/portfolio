6c28fcadc22731460d461149a587e948153c48ab
Rahul Lakhchaura <rlakhchaura3@gmail.com>
2024-08-01 19:11:07 +0530

    v4

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -1,10 +1,18 @@
 var typed = new Typed('#element1', {
-    strings: ['<i>Front-End Developer</i>'],
-    typeSpeed: 40,
-  });
+  strings: ['<i>Front-End Developer</i>'],
+  typeSpeed: 40,
+  loop: true,
+  backDelay: 1000,
+});
 
-  var typed = new Typed('#element2', {
-    strings: ['ReactJs', ' AngularJS', 'Next.js'],
-    typeSpeed: 50,
+var typed = new Typed('#element2', {
+  strings: ['ReactJs', ' AngularJS', 'Next.js'],
+  typeSpeed: 50,
+  loop: true,
+  backDelay: 900,
+});
+
+var typed = new Typed('#element3', {
+  strings: ['Rahul Lakhchaura'],
+  typeSpeed: 30,
   });
-  
