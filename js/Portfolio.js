347d38107fdd467ff36737e8cb3ed550c371eec1
Rahul Lakhchaura <rlakhchaura3@gmail.com>
2024-08-01 11:14:06 +0530

    v2

---
+++ js/Portfolio.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#element1', {
+    strings: ['<i>Front-End Developer</i>'],
+    typeSpeed: 20,
+  });
+
+  var typed = new Typed('#element2', {
+    strings: ['ReactJs', ' AngularJS', 'Next.js'],
+    typeSpeed: 50,
+  });
+  
