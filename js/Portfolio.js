85c69ca4521de163e418232bce5609811578b24e
Rahul Lakhchaura <rlakhchaura3@gmail.com>
2024-08-25 23:34:48 +0530

    v1.2

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -16,36 +16,3 @@ var typed = new Typed('#element3', {
   strings: ['Rahul Lakhchaura'],
   typeSpeed: 30,
 });
-
-function toggleText() {
-  const moreText = document.getElementById("moreText");
-  const btnText = document.getElementById("readMoreBtn");
-
-  if (moreText.style.display === "none" || moreText.style.display === "") {
-    moreText.style.display = "inline";
-    btnText.innerHTML = "Read Less";
-  } else {
-    moreText.style.display = "none";
-    btnText.innerHTML = "Read More";
-  }
-}
-
-// Apply initial state based on screen size
-function applyInitialState() {
-  const moreText = document.getElementById("moreText");
-  const btnText = document.getElementById("readMoreBtn");
-
-  if (window.innerWidth >= 426) {
-    moreText.style.display = "inline";
-    btnText.style.display = "block";
-  } else {
-    moreText.style.display = "none";
-    btnText.style.display = "block";
-  }
-}
-
-// Apply the correct state when the page loads
-applyInitialState();
-
-// Attach an event listener to handle window resize
-window.addEventListener('resize', applyInitialState);
